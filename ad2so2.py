ae03b4ebfed57f4bef3dc308d6d38bda0be803e9
alanlcurry <53018807+alanlcurry@users.noreply.github.com>
2025-10-04 21:19:01 -0500

    Rewrite to Class

--- ad2so2.py
+++ ad2so2.py
@@ -1,3 +1,43 @@
+#!/usr/local/bin/python3
+
+# MIT License
+
+# Copyright (c) 2023  Alan L. Curry
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# This routine reads a Affinity Designer 2 generated SVG file and adds Shaper Tools shaper: 
+# attributes if present in the serif:id
+
+# usage: ad2so.py [-h] -I INFILE -O OUTFILE [-G [GBLATTR ...]]
+
+# Shaper Origin Support for AD2
+
+# options:
+#   -h, --help            show this help message and exit
+#   -i INFILE, --inFile INFILE
+#                         input SVG file
+#   -o OUTFILE, --outFile OUTFILE
+#                         output SVG file
+#   -g [GBLATTR ...], --gblAttr [GBLATTR ...]
+#                         input global shaper attributes (optional)
+
 import xml.etree.ElementTree as ET
 import argparse
 import sys
@@ -126,10 +166,22 @@ class ShaperSVGProcessor:
             color_name = fillValue
         else:
             color_name = "black"
-        cutType = cutTypeColors[color_name]
-        fillValue = cutTypefillValues[cutType]
-        elem.set("shaper:cutType", cutType)
-        elem.set("fill", fillValue)
+
+        try:
+            cutType = cutTypeColors[color_name]
+        except KeyError:
+            cutType = None
+        finally:
+            if cutType: 
+                elem.set("shaper:cutType", cutType)
+
+        try:
+            fillValue = cutTypefillValues[cutType]
+        except KeyError:
+            fillValue = None
+        finally:
+            if fillValue:
+                elem.set("fill", fillValue)
 
     def svg_add_other_attributes(self, elem, attributes):
         elemKeys = elem.keys()
